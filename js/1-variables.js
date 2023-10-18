1957b5652ed1cc46b5953bc450bc5596c3e0fa79
Maximiliano Gomez <alexander34223@gmail.com>
2023-10-18 19:01:50 -0300

    ejercio de concatenacion

--- js/1-variables.js
+++ js/1-variables.js
@@ -1,6 +1,13 @@
+// ejercicio 1 creacion de variables 
 let num1 = 7;
 let num2 = 8;
 console.log(num1+num2);
 console.log(num1-num2);
 console.log(num1*num2);
 console.log(num1/num2);
+
+// Ejercicio 2 concatenacion 
+let nombre =" Maximiliano Ariel "
+let apellido = "Gomez"
+let nombreYapellido =( nombre + apellido )
+console.log(nombreYapellido) 
