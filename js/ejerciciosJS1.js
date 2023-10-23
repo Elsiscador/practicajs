6df9fd7f7102342b5c87fa70d90f8646a401d26c
Maximiliano Gomez <alexander34223@gmail.com>
2023-10-23 17:10:10 -0300

    ejercicios del 1 al 5 terminado

---
+++ js/ejerciciosJS1.js
@@ -0,0 +1,13 @@
+// Ejercio 1 mensaje  de alert
+alert("seguro rompiste todo y vas decir que no hiciste nada ,tipico de un programador novato")
+// Ejercicio 2 texto en pantalla 
+document.write("<h1>Hello World </h1> " );
+
+
+// ejercicio 4
+let nombreUsuario = prompt("Ingrese su nombre");
+console.log("Hola " + nombreUsuario);
+//ejercicio 5
+let numero1 = parseFloat(prompt("Ingrese el primer número"))
+let numero2 = parseFloat(prompt("Ingrese el segundo número"));
+console.log("La suma de los números es: " + (numero1 + numero2));
