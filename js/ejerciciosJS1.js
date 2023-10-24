3eea563c6db36342b219343de4a0033131af0f42
Maximiliano Gomez <alexander34223@gmail.com>
2023-10-23 23:36:32 -0300

    cambie los mensj de consola y agregue ejercicio 9

--- js/ejerciciosJS1.js
+++ js/ejerciciosJS1.js
@@ -22,28 +22,28 @@ let numero3 = parseInt(prompt("Adivinaremos cual es el mayor de los 2 numeros qu
 let numero4 = parseInt(prompt("Adivinaremos cual es el mayor de los 2 numeros que elijas ahora"));
 
 if (numero3 > numero4) {
-  console.log('El primer numero que es ' + numero3 + ' es mayor que ' + numero4);
+  console.log('El  numero  ' +numero3+ ' es mayor que ' + numero4);
 } else {
-  console.log('El segundo numero que es' + numero4 + ' es mayor que ' + numero3 )
+  console.log('El  numero  ' +numero4+ ' es mayor que ' + numero3 )
 }
 // Ejercicio 7
-let num3 = parseInt(prompt('Adivinaremos cual es el mayor de los 3 numeros que elijas ahora'));
-let num4 = parseInt(prompt('Adivinaremos cual es el mayor de los 3 numeros que elijas ahora'))
-let num5 =parseInt(prompt('Adivinaremos cual es el mayor de los 3 numeros que elijas ahora'))
+let num3 = parseInt(prompt('Adivinaremos cual es el mayor de los 3 numeros que elijas Empezemos'));
+let num4 = parseInt(prompt('Elije otro numero'))
+let num5 =parseInt(prompt('Ahora elije el ultimo numero y te diremos cual es el mayor'))
 
 if (num3 > num4 && num3 > num5){ 
-    console.log("El primer número de los 3 es el mayor");
+    console.log('El numero '+num3+' es el mayor de los 3 que elejiste' );
 }else if (num4 > num3 && num4 > num5){
-    console.log("El segundo número de los 3 es el mayor");
+    console.log('El numero '+num4+' es el mayor de los 3 que elejiste' );
 } else{
-    console.log('El tercer numero de los 3 es mayor');
+    console.log('El numero '+num5+' es el mayor de los 3 que elejiste' );
 }
 
 // ejercicio 8
 let numero = parseFloat(prompt("Ingrese un número y te diremos si es divisible por 2"));
 
 if (numero % 2 === 0) {
-  console.log("El número es divisible por 2");
+  console.log('El número '+numero+' es divisible por 2');
 } else {
   console.log("El número no es divisible por 2");
 }
@@ -52,9 +52,9 @@ let frase = prompt("Ingrese una frase");
 let vocales = "";
 
 for (let i = 0; i < frase.length; i++) {
-  let caracter = frase[i].toLowerCase();
-  if (caracter === "a" || caracter === "e" || caracter === "i" || caracter === "o" || caracter === "u") {
-    vocales += caracter + " ";
+  let vocal = frase[i];
+  if (vocal === "a" || vocal === "e" || vocal === "i" || vocal === "o" || vocal === "u") {
+    vocales += vocal + " ";
   }
 }
 
